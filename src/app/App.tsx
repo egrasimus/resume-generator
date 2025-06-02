dbb5b524ffa103c3ccd0efb81a218ebc9f525f36
Egor <73159143+egrasimus@users.noreply.github.com>
2025-06-02 13:54:15 +0300

    initial coomit

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { ResumePage } from "@/pages/resume"
+
+export const App = () => {
+	return (
+		<div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+			<ResumePage />
+		</div>
+	)
+}
---
+++ src/pages/resume/ui.tsx
@@ -0,0 +1,7 @@
+export const ResumePage = () => {
+	return (
+		<main>
+			<h1>Resume Generator</h1>
+		</main>
+	)
+}
