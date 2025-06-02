72501915ca1d652b4228431a1af8b6225d0ee1cb
Egor <73159143+egrasimus@users.noreply.github.com>
2025-06-02 16:00:00 +0300

    micro fix

--- src/features/download-pdf/ui/DownloadButton.tsx
+++ src/features/download-pdf/ui/DownloadButton.tsx
@@ -1,10 +1,15 @@
 import React from "react"
 import { generatePdf } from "../lib/generatePdf"
+import styles from "./DownloadButton.module.scss"
 
 export const DownloadButton = () => {
 	const handleClick = () => {
 		generatePdf("resume-preview")
 	}
 
-	return <button onClick={handleClick}>Скачать PDF</button>
+	return (
+		<div className={styles.wrapper}>
+			<button onClick={handleClick}>Скачать PDF</button>
+		</div>
+	)
 }
